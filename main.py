b37ebbefabe212c49bf2715a168fedeef7687ff7
Thomas Atkins <thomaskarlatkins@gmail.com>
2018-11-19 19:54:34 -0600

    Error handling for nxt connection

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from nxt.motor import *
 
 class NXT:
     def __init__(self):
-        b = nxt.locator.find_one_brick()
+        print("Connecting to NXT...")
+        try:
+            b = nxt.locator.find_one_brick()
+        except nxt.locator.BrickNotFoundError as error:
+            print("NXT not found.")
+            exit()
+        print("Connected!")
         self.m_left = Motor(b, PORT_B)
         self.m_right = Motor(b,PORT_C)
 
